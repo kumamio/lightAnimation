10978716bc807e425ad6446a2eef07af0bb5928d
sunshine <tpelf@Hotmail.com>
2017-04-28 10:20:08 +0800

    .
    .

--- index.js
+++ index.js
@@ -12,10 +12,8 @@
 
     var curves=document.getElementById("curves");
     var ctxCurves = curves.getContext("2d");
-    var ratioCurvesX;
-    var ratioCurvesY;
 
-    var city=document.getElementById("scene_city");
+    var city = document.getElementById("scene_city");
     city.width = clientW;
     city.height = clientH;
     var ctx = city.getContext("2d");
@@ -25,15 +23,12 @@
     sky.height = clientH;
     var skyctx = sky.getContext("2d");
 
-    var sun=document.getElementById("sun");
-    sun.style.width=clientW/12+"px";
-    sun.style.height=clientW/12+"px";
-    sun.style.top=clientH+"px";
+    var sun = document.getElementById("sun");
+    sun.style.width = clientW/12 + "px";
+    sun.style.height = clientW/12 + "px";
+    sun.style.top = clientH + "px";
 
     var radius = 0;
-
-    document.getElementById("body").width=clientW;
-    document.getElementById("body").height=clientH;
 
 
     var drawCover = function(){
@@ -42,10 +37,10 @@
         ctxCover.beginPath();
         ctxCover.fillStyle = '#444';
         ctxCover.fillRect(0,0,clientW,clientH);
-        ctxCover.textAlign="center";
+        ctxCover.textAlign = "center";
 
-        ctxCover.font=clientW/4+"px Impact";
-        ctxCover.textBaseline="middle";
+        ctxCover.font = clientW/4 + "px Impact";
+        ctxCover.textBaseline = "middle";
         ctxCover.globalCompositeOperation = 'destination-out';
         ctxCover.shadowColor = "rgba(0,0,0,.5)";
         ctxCover.fillText("color",clientW/2,clientH/2);
@@ -53,23 +48,23 @@
         ctxCover.globalCompositeOperation = 'source-over';//amazing!
     }
 
-    var newPointList=[];
+    var newPointList = [];
     var light = 0;
 
 
     //随着light变化生成新的hsl色值
-    var newHsl = function(arr) {
-      return "hsl("+arr[0]+","+arr[1]+"%,"+parseInt(light/100*arr[2])+"% )"
+    var newHsl = function (arr) {
+      return "hsl(" + arr[0] + "," + arr[1] + "%," + parseInt(light/100 * arr[2]) + "% )"
     }
 
     //把原先的点转化成可以生成贝塞尔曲线的点
     var changePoint = function (arr,i,origin){
-      if (arr.length==6) {
-        arr = arr.map(function(value, index){
+      if (arr.length == 6) {
+        arr = arr.map(function (value, index){
           return value + origin[(index % 2 == 0) ? 0 : 1];
         })
       }else{
-        if(arr[6]=="C"){
+        if(arr[6] == "C"){
           arr.length = 6;
         }
       }
@@ -79,11 +74,11 @@
     }
 
     //点到bezier曲线
-    var pointToCurve = function(pointlist,ctx,origin) {
-      Array.prototype.map.call(pointlist, function(value,index){
-        var innerArr = value.map(function(v,i){
-          if ( i<=5 ) {
-            return (i % 2 == 0) ? v*ratioX : v*ratioY;
+    var pointToCurve = function (pointlist,ctx,origin) {
+      Array.prototype.map.call(pointlist, function (value,index){
+        var innerArr = value.map(function (v,i) {
+          if ( i <= 5 ) {
+            return (i % 2 == 0) ? v * ratioX : v * ratioY;
           }else{
             return v;
           }
@@ -98,7 +93,7 @@
     //画曲线并着色
     var drawBezier = function(origin,pointlist,color,ctx){
       ctx.beginPath();
-      origin=[origin[0]*ratioX,origin[1]*ratioY];
+      origin = [origin[0] * ratioX,origin[1] * ratioY];
       ctx.moveTo( origin[0], origin[1]);
       pointToCurve ( pointlist, ctx, origin );
       ctx.fillStyle = newHsl(color);
@@ -148,30 +143,30 @@
     //画天空背景
     function drawBG(h,s,l){
       sky.style.display = "block";
-      skyctx.fillStyle = "hsl("+h+", "+s+"%, "+ l*light/100+ "%)";
+      skyctx.fillStyle = "hsl(" + h + ", " + s + "%, " + l * light/100 + "%)";
       skyctx.fillRect(0,0,clientW,clientH);
     }
     
     //画太阳
     function drawSun(h,s,l){
-      sun.style.display= "block";
-      sun.style.backgroundColor = "hsl("+h*light/100+", "+s+"%, "+ l+ "%)"
-      sun.style.top=clientH*0.8*(1.1-light/100)+"px";
-      sun.style.left=clientW*0.5*light/100+"px";
+      sun.style.display = "block";
+      sun.style.backgroundColor = "hsl(" + h * light/100 + ", " + s + "%, " + l + "%)"
+      sun.style.top = clientH * 0.8 * (1.1 - light/100) + "px";
+      sun.style.left = clientW * 0.5 * light/100+"px";
     }
 
 
     //画矩形
     var drawRect = function(x,y,h,s,l,width,height){
-      ctx.fillStyle = "hsl("+h+", "+s+"%, "+ l*light/100+ "%)";
-      ctx.fillRect(x*ratioX,y*ratioY,width*ratioX,height*ratioY);
+      ctx.fillStyle = "hsl(" + h + ", " + s + "%, " + l * light/100 + "%)";
+      ctx.fillRect(x * ratioX,y * ratioY,width * ratioX,height * ratioY);
     }
 
     //画窗户
     var drawWindows = function(x,y,width,height){
-      for (var j = 0; j < 4; j++) {
-        for (var i = 0; i < 2; i++) {
-          drawRect(x+width*2*i,y+height*1.5*j,56,94,50,width,height,ctx)
+      for (var j = 0; j < 4; j ++) {
+        for (var i = 0; i < 2; i ++) {
+          drawRect(x + width * 2 * i,y + height * 1.5 * j,56,94,50,width,height,ctx)
         };
 
       }
@@ -226,18 +221,15 @@
       grd.addColorStop(0.8, grassColor1);
       grd.addColorStop(1,   grassColor1);
       ctx.fillStyle = grd;
-      ctx.fillRect(0,0.8*clientH,clientW,clientH);  
+      ctx.fillRect(0,0.8 * clientH,clientW,clientH);  
 
     }
 
     var init = function (){
-      // debugger;
+
       drawCover();
 
       cover.addEventListener('mousedown',function(event){
-
-        var x = event.pageX;
-        var y = event.pageY;
 
          //点击时画圈
          (function drawCircle() {
@@ -245,7 +237,7 @@
             ctxCover.beginPath();
             ctxCover.arc(event.pageX, event.pageY, radius, 0, 2 * Math.PI);
             ctxCover.fill();
-            if (radius<=clientW) {
+            if (radius <= clientW) {
               setTimeout(function(){
                 requestAnimationFrame(drawCircle);
               },10)
@@ -254,22 +246,22 @@
 
               //触发第二幅动画
               (function drawScene() {
-                document.getElementById("scene_city").style.display="block";
-                light=light +1;
+                document.getElementById("scene_city").style.display = "block";
+                light = light + 1;
 
-                if (light<=100) {
-                  drawSun(47,83,60);
-                  drawBG(225,97,77);
+                if (light <= 100) {
+                  drawBG(225,97,77);//画天空
+                  drawSun(47,83,60);//画太阳
                   drawCurvesCanvas();//画曲线物体
                   drawRect(1152,740,10,97,14,22,60);//画树杆
                   drawRect(1305,762,10,97,14,18,56);
-                  drawTree(ctx, 1101*ratioX, 568*ratioY, 124*ratioX, 198*ratioY);//画树枝
-                  drawTree(ctx, 1268*ratioX, 628*ratioY, 94*ratioX, 149*ratioY);
+                  drawTree(ctx, 1101 * ratioX, 568 * ratioY, 124 * ratioX, 198 * ratioY);//画树枝
+                  drawTree(ctx, 1268 * ratioX, 628 * ratioY, 94 * ratioX, 149 * ratioY);
                   drawRect(366,313,0,0,35,237,490);//画两栋建筑
                   drawRect(705,414,0,0,35,186,386);
                   drawWindows(418,365,44,70);//建筑上的窗户
                   drawWindows(749,453,33,54);
-                  drawGrass(226,92,80);
+                  drawGrass(226,92,80);//画草地
 
                   setTimeout(function(){
                     requestAnimationFrame(drawScene);
